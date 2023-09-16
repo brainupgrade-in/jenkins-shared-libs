a202b39ace8fb1ef961ee9fcdd1da7781da16034
Rajesh G <rajesh@brainupgrade.in>
2023-09-16 19:36:21 +0530

    refine

--- vars/jenkinsForJava.groovy
+++ vars/jenkinsForJava.groovy
@@ -1,42 +1,44 @@
 #!groovy
+/* groovylint-disable MethodReturnTypeRequired, NoDef */
 def call(Map config) {
-  pipeline {
-       agent any
-       tools {
-           maven 'mvn'
-       }
-       stages {
-           stage("Tools initialization") {
-               steps {
-                   sh "mvn --version"
-                   sh "java -version"
-               }
-           }
-           stage("Checkout Code") {
-               steps {
-                   git branch: "${config.branch}",
+    pipeline {
+        agent any
+        tools {
+            maven 'mvn'
+        }
+        stages {
+            stage('Tools initialization') {
+                steps {
+                    sh 'mvn --version'
+                    sh 'java -version'
+                }
+            }
+            stage('Checkout Code') {
+                steps {
+                    git branch: "${config.branch}",
                        url: "${config.repoUrl}"
-               }
-           }
-           stage("Cleaning workspace") {
-               steps {
-                   sh "mvn clean"
-               }
-           }
-           stage("Running Testcase") {
-              steps {
-                   sh "mvn test"
-               }
-           }
-           stage("Packing Application") {
-               steps {
-                   sh "mvn package -DskipTests"
-               }
-           }
-       }
-   }
+                }
+            }
+            stage('Cleaning workspace') {
+                steps {
+                    sh 'mvn clean'
+                }
+            }
+            stage('Running Testcase') {
+                steps {
+                    sh 'mvn test'
+                }
+            }
+            stage('Packing Application') {
+                steps {
+                    sh 'mvn package -DskipTests'
+                }
+            }
+        }
+    }
 }
 //Jenkins Controller (import this repo as global lib using Jenkins settings)
 //Jenkinsfile
 //@Library('brainupgrade-shared-libs') _
 //jenkinsForJava (repoUrl: 'https://github.com/brainupgrade-in/spring-framework-petclinic.git', branch: 'main' )
+

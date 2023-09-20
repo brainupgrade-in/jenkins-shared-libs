b9c6e8547a3eb7c8345ecf090ed68a043551f3db
Rajesh G <rajesh@brainupgrade.in>
2023-09-21 01:39:14 +0530

    refine

--- vars/buildPlugin.groovy
+++ vars/buildPlugin.groovy
@@ -1,7 +1,6 @@
 def call(Map config) {
-    node {
-        git url: "https://github.com/jenkinsci/${config.name}-plugin.git"
-        sh 'mvn install'
-    }
+
+    git url: "https://github.com/jenkinsci/${config.name}-plugin.git"
+    sh 'mvn install'
 }
 //Jenkinsfile-scripted buildPlugin name: 'git'
